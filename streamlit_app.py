924b09c49277f74d0a3363fce17a1be99c06af32
Overtri <77598823+Overtri@users.noreply.github.com>
2024-02-02 15:49:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,5 @@ add_my_fruit = streamlit.text_input('What fruit would you like add?','Jackfruit'
 streamlit.write('The user entered ', add_my_fruit)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values('test')")
