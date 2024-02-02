48cc7261a4cb8cb1a93b0a20356d29a0215a9764
Overtri <77598823+Overtri@users.noreply.github.com>
2024-02-02 15:51:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,4 @@ streamlit.write('The user entered ', add_my_fruit)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ add_my_fruit)
 
-my_cur.execute("insert into fruit_load_list values('test')")
+my_cur.execute("insert into fruit_load_list values('from streamlit')")
